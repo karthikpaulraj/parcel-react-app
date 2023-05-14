101f186ec9ed952df17760d2ad6c9eecd9bc6778
karthikpaul <leo9karthik@gmail.com>
2023-05-14 21:25:46 +0530

    Added redux toolkit in the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+/* Plugin */
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+/* Component */
+import SideMenu from "./SideMenu";
+
 const Header = () => {
-  return <div>Header</div>;
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <>
+      <div>Header</div>
+      <button onClick={toggleMenuHandler}>Toggle menu</button>
+      <SideMenu />
+    </>
+  );
 };
 
 export default Header;
